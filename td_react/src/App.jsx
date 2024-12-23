a645bf707e33bb79146dd3168fcfd7623fc5fecd
Oussama EL RHRIB <oussamastr710@gmail.com>
2024-12-23 16:27:21 +0100

    TD01 : Ajouter image

--- td_react/src/App.jsx
+++ td_react/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import university from './assets/azurelogo.png'
 import './App.css'
-function Header({title1, title2}) {
-  
+function Header({ title1, title2, logo }) {
   return (
     <header>
+      <img src={logo}  alt="Logo" />
       <h1>{title1}</h1>
       <h2>{title2}</h2>
     </header>
-  )
+  );
 }
 function MainContent() {
   return (
@@ -36,7 +37,7 @@ function App() {
   return (
     <>
       <div>
-        <Header title1='Introduction à React'  title2='A la découverte des premières notions de React'/>
+        <Header title1='Introduction à React'  title2='A la découverte des premières notions de React' logo={university}/>
         <MainContent />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
