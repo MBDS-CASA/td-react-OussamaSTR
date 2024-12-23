30c22e11f6f54cff110e1faa884d3d8f8e2b46d1
Oussama EL RHRIB <oussamastr710@gmail.com>
2024-12-23 16:09:56 +0100

    TD01 : Ajouter un footer

--- td_react/src/App.jsx
+++ td_react/src/App.jsx
@@ -20,6 +20,15 @@ function MainContent() {
     </main>
   )
 }
+function Footer({nom, prenom}) {
+  return (
+    <footer>
+      
+        Tous droits réservés - {nom} {prenom}
+    
+    </footer>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -54,6 +63,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
+        <Footer nom='EL RHRIB' prenom='Oussama'/>
+      
     </>
   )
 }
